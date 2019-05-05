4fc1f9b6fb426d4d8b1a6edfbe604c21354752dc
AndroidDeveloperLB <AndroidDeveloperLB5020914>
2019-05-06 01:36:32 +0300

    updated sdks

--- app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
+++ app/src/main/java/com/github/chagall/notificationlistenerexample/NotificationListenerExampleService.java
@@ -1,21 +1,24 @@
 package com.github.chagall.notificationlistenerexample;
 
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 /**
  * MIT License
- *
- *  Copyright (c) 2016 Fábio Alves Martins Pereira (Chagall)
- *
+ * <p>
+ * Copyright (c) 2016 Fábio Alves Martins Pereira (Chagall)
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -54,28 +57,37 @@ public class NotificationListenerExampleService extends NotificationListenerServ
     }
 
     @Override
-    public void onNotificationPosted(StatusBarNotification sbn){
+    public void onCreate() {
+        super.onCreate();
+        final ClipboardManager manager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        manager.addPrimaryClipChangedListener(new ClipboardManager.OnPrimaryClipChangedListener() {
+            @Override
+            public void onPrimaryClipChanged() {
+                Log.d("AppLog", "changed to:" + manager.getText());
+                // or this for the textview:
+                // textView.setText(manager.getText());
+            }
+        });
+    }
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
         int notificationCode = matchNotificationCode(sbn);
-
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE){
-            Intent intent = new  Intent("com.github.chagall.notificationlistenerexample");
+        if (notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
+            Intent intent = new Intent("com.github.chagall.notificationlistenerexample");
             intent.putExtra("Notification Code", notificationCode);
             sendBroadcast(intent);
         }
     }
 
     @Override
-    public void onNotificationRemoved(StatusBarNotification sbn){
+    public void onNotificationRemoved(StatusBarNotification sbn) {
         int notificationCode = matchNotificationCode(sbn);
-
-        if(notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
-
+        if (notificationCode != InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE) {
             StatusBarNotification[] activeNotifications = this.getActiveNotifications();
-
-            if(activeNotifications != null && activeNotifications.length > 0) {
+            if (activeNotifications != null && activeNotifications.length > 0) {
                 for (int i = 0; i < activeNotifications.length; i++) {
                     if (notificationCode == matchNotificationCode(activeNotifications[i])) {
-                        Intent intent = new  Intent("com.github.chagall.notificationlistenerexample");
+                        Intent intent = new Intent("com.github.chagall.notificationlistenerexample");
                         intent.putExtra("Notification Code", notificationCode);
                         sendBroadcast(intent);
                         break;
@@ -87,19 +99,15 @@ public class NotificationListenerExampleService extends NotificationListenerServ
 
     private int matchNotificationCode(StatusBarNotification sbn) {
         String packageName = sbn.getPackageName();
-
-        if(packageName.equals(ApplicationPackageNames.FACEBOOK_PACK_NAME)
-                || packageName.equals(ApplicationPackageNames.FACEBOOK_MESSENGER_PACK_NAME)){
-            return(InterceptedNotificationCode.FACEBOOK_CODE);
-        }
-        else if(packageName.equals(ApplicationPackageNames.INSTAGRAM_PACK_NAME)){
-            return(InterceptedNotificationCode.INSTAGRAM_CODE);
-        }
-        else if(packageName.equals(ApplicationPackageNames.WHATSAPP_PACK_NAME)){
-            return(InterceptedNotificationCode.WHATSAPP_CODE);
-        }
-        else{
-            return(InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE);
+        if (packageName.equals(ApplicationPackageNames.FACEBOOK_PACK_NAME)
+                || packageName.equals(ApplicationPackageNames.FACEBOOK_MESSENGER_PACK_NAME)) {
+            return (InterceptedNotificationCode.FACEBOOK_CODE);
+        } else if (packageName.equals(ApplicationPackageNames.INSTAGRAM_PACK_NAME)) {
+            return (InterceptedNotificationCode.INSTAGRAM_CODE);
+        } else if (packageName.equals(ApplicationPackageNames.WHATSAPP_PACK_NAME)) {
+            return (InterceptedNotificationCode.WHATSAPP_CODE);
+        } else {
+            return (InterceptedNotificationCode.OTHER_NOTIFICATIONS_CODE);
         }
     }
 }
